c646a242363338a62e3653abb7474bb724cf4dae
Марина Звягина <zvyagina.marina@gmail.com>
2017-06-29 19:42:30 +0300

    Moved buttons initialisation to "initButtons" function

--- testAnimation/ViewController.m
+++ testAnimation/ViewController.m
@@ -10,51 +10,43 @@
 
 @interface ViewController ()
 @property (nonatomic, strong) UIView * visibleView;
+@property (nonatomic, strong) NSArray <UIButton *> *buttons;
 @end
+
+static NSUInteger countOfButtons = 5;
 
 @implementation ViewController
 
+-(void) initButtons {
+    NSArray <UIColor *> * colors = @[UIColor.blueColor, UIColor.grayColor, UIColor.redColor, UIColor.yellowColor, UIColor.orangeColor];
+    NSMutableArray <UIButton *> *currentButtons = [NSMutableArray<UIButton *> new];
+    
+    for (NSUInteger i = 0; i<countOfButtons; i++) {
+        UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(i*50 + 20, 20, 40, 50)];
+        button.backgroundColor = colors[i];
+        [currentButtons addObject:button];
+        [self.view addSubview:button];
+    }
+    self.buttons = [[NSArray<UIButton *> alloc] initWithArray:currentButtons];;
+}
 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(20, 20, 40, 50)];
-    button.backgroundColor = [UIColor blueColor];
-    [button addTarget:self action:@selector(makeInvisible) forControlEvents:UIControlEventTouchUpInside];
-    
-    UIButton *button2 = [[UIButton alloc] initWithFrame:CGRectMake(100, 20, 40, 50)];
-    button2.backgroundColor = [UIColor grayColor];
-    [button2 addTarget:self action:@selector(makeVisible) forControlEvents:UIControlEventTouchUpInside];
-    
-    UIButton *button3 = [[UIButton alloc] initWithFrame:CGRectMake(180, 20, 40, 50)];
-    button3.backgroundColor = [UIColor redColor];
-    [button3 addTarget:self action:@selector(rotateView) forControlEvents:UIControlEventTouchUpInside];
-    
-    UIButton *button4 = [[UIButton alloc] initWithFrame:CGRectMake(260, 20, 40, 50)];
-    button4.backgroundColor = [UIColor yellowColor];
-    [button4 addTarget:self action:@selector(moveView) forControlEvents:UIControlEventTouchUpInside];
-    
-    UIButton *button5 = [[UIButton alloc] initWithFrame:CGRectMake(340, 20, 40, 50)];
-    button5.backgroundColor = [UIColor orangeColor];
-    [button5 addTarget:self action:@selector(groupAnimation) forControlEvents:UIControlEventTouchUpInside];
-    
-    
+
+    [self initButtons];
+
+    [self.buttons[0] addTarget:self action:@selector(makeInvisible) forControlEvents:UIControlEventTouchUpInside];
+    [self.buttons[1] addTarget:self action:@selector(makeVisible) forControlEvents:UIControlEventTouchUpInside];
+    [self.buttons[2] addTarget:self action:@selector(rotateView) forControlEvents:UIControlEventTouchUpInside];
+    [self.buttons[3] addTarget:self action:@selector(moveView) forControlEvents:UIControlEventTouchUpInside];
+    [self.buttons[4] addTarget:self action:@selector(groupAnimation) forControlEvents:UIControlEventTouchUpInside];
     
     self.visibleView = [[UIView alloc] initWithFrame:CGRectMake(100, 170, 100, 100)];
     self.visibleView.backgroundColor = [UIColor greenColor];
     
     self.view.backgroundColor = [UIColor whiteColor];
-    
-    [self.view addSubview:button];
-    [self.view addSubview:button2];
-    [self.view addSubview:button3];
-    [self.view addSubview:button4];
-    [self.view addSubview:button5];
-    
     [self.view addSubview:self.visibleView];
-    
-
 }
 
 -(void) makeInvisible {
@@ -74,17 +66,14 @@
     [UIView animateWithDuration:3.0 animations:^{
         self.visibleView.transform = transform;
     }];
-    
 }
 
 -(void) moveView {
-    
     CGRect boundingRect = CGRectMake(-150, -150, 300, 300);
     
     CAKeyframeAnimation *orbit = [CAKeyframeAnimation animation];
     orbit.keyPath = @"position";
-    orbit.path = CFAutorelease(CGPathCreateWithRect(boundingRect, NULL));//(CGPathCreateWithEllipseInRect(boundingRect, NULL));
-    orbit.duration = 4;
+    orbit.path = CFAutorelease(CGPathCreateWithRect(boundingRect, NULL));    orbit.duration = 4;
     orbit.additive = YES;
     orbit.repeatCount = HUGE_VALF;
     orbit.calculationMode = kCAAnimationPaced;
